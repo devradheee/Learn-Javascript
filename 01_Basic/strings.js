3a49a2a9fe1eee6572805ec7d40ae4ae795cd0a4
RADHESHYAM KUSHWAHA <104156567+devradheee@users.noreply.github.com>
2023-12-26 20:35:16 +0000

    string

---
+++ 01_Basic/strings.js
@@ -0,0 +1,33 @@
+const name = "Radheshyam"
+const repoCount = 50
+
+console.log(`Hello my name is ${name} and my repo count is $ {repoCont}`);
+
+const gameName = new String('devradheee')
+
+// console.log(gamrName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = " devradheee   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://devradheee.com/radhe520devradheee"
+
+console.log(url.replace('%20', '-'))
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'));
+
+
